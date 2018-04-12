fd869f478cc3db3dae51a1a6b9c0d4f4367e64e1
Qinz <719263987@qq.com>
2018-04-12 11:30:51 +0800

    上传基于python3.6的脚本

---
+++ AutoPage/DaoBao 3.6.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+#coding=utf-8 
+import os
+import requests
+import webbrowser
+import subprocess
+import shutil
+
+appFileFullPath = '/Users/Qinz/Library/Developer/Xcode/DerivedData/DDDS-exihsnwhearstpfrwwzohuclewrx/Build/Products/Debug-iphoneos/DDDS.app'
+PayLoadPath = '/Users/Qinz/Desktop/Payload'
+packBagPath = '/Users/Qinz/Desktop/ProgramBag'
+openUrlPath = 'https://www.pgyer.com/manager/dashboard/app/40c633aa8dc0ba15191632860558825e'
+
+
+#上传蒲公英
+USER_KEY = "61ded40a68afef20ab4b2e9fd278acd2"
+API_KEY = "a4fe2724dc6d8c7eb9f0f1994eec219e"
+
+#上传蒲公英
+def uploadIPA(IPAPath):
+    if(IPAPath==''):
+        print("\n*************** 没有找到对应上传的IPA包 *********************\n")
+        return
+    else:
+        print("\n***************开始上传到蒲公英*********************\n")
+        url='http://www.pgyer.com/apiv1/app/upload'
+        data={
+            'uKey':USER_KEY,
+            '_api_key':API_KEY,
+            'installType':'2',
+            'password':'',
+            'updateDescription':""
+        }
+        files={'file':open(IPAPath,'rb')}
+        r=requests.post(url,data=data,files=files)
+
+def openDownloadUrl():
+    webbrowser.open(openUrlPath,new=1,autoraise=True)
+    print ("\n*************** 更新成功 *********************\n")
+
+#编译打包流程
+def bulidIPA():
+    
+    #删除之前打包的ProgramBag文件夹
+    subprocess.call(["rm","-rf",packBagPath])
+    #创建PayLoad文件夹
+    mkdir(PayLoadPath)
+    #将app拷贝到PayLoadPath路径下
+    subprocess.call(["cp","-r",appFileFullPath,PayLoadPath])
+    #在桌面上创建packBagPath的文件夹
+    subprocess.call(["mkdir","-p",packBagPath])
+    #将PayLoadPath文件夹拷贝到packBagPath文件夹下
+    subprocess.call(["cp","-r",PayLoadPath,packBagPath])
+    #删除桌面的PayLoadPath文件夹
+    subprocess.call(["rm","-rf",PayLoadPath])
+    #切换到当前目录
+    os.chdir(packBagPath)
+    #压缩packBagPath文件夹下的PayLoadPath文件夹夹
+    subprocess.call(["zip","-r","./Payload.zip","."])
+    print ("\n*************** 打包成功 *********************\n")
+    #将zip文件改名为ipa
+    subprocess.call(["mv","payload.zip","Payload.ipa"])
+    #删除payLoad文件夹
+    subprocess.call(["rm","-rf","./Payload"])
+
+
+#创建PayLoad文件夹
+def mkdir(PayLoadPath):
+    isExists = os.path.exists(PayLoadPath)
+    if not isExists:
+        os.makedirs(PayLoadPath)
+        print(PayLoadPath + '创建成功')
+        return True
+    else:
+        print (PayLoadPath + '目录已经存在')
+        return False
+
+
+if __name__ == '__main__':
+    des = input("请输入更新的日志描述:")
+    bulidIPA()
+    #uploadIPA('%s/Payload.ipa'%packBagPath)
+    #openDownloadUrl()
+
+
+    
+
+
+
+    
+
